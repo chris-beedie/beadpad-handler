88d67e1590f41a9670d5cc2b3b349c9016490bfe
Chris Beedie <chris.beedie@ghd.com>
2020-09-23 23:33:53 +0100

    display mode name in print message

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ import time
 
 from enum import IntEnum
 
-# Using enum class create enumerations
+
 class Mode(IntEnum):
     Teams = 0
     Sonos = 1
     Another = 2
 
-
 keypad = Keypad(Mode)
 
+
 def key_pressed(mode: Mode, key: Key):
-    print(f"START: mode: {mode}, Key: {key}")
+    print(f"START: mode: {mode.name}, Key: {key}")
     time.sleep(2)
-    print(f"END: mode: {mode}, Key: {key}")
+    print(f"END: mode: {mode.name}, Key: {key}")
+
+
 
 
 keypad.start(key_pressed)

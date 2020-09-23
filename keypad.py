50af3f9daaf700cf9268209affa2a1aee98873b0
Chris Beedie <chris.beedie@ghd.com>
2020-09-23 23:27:12 +0100

    refactored to class

--- keypad.py
+++ keypad.py
@@ -1,9 +1,8 @@
-from enum import Enum, auto
+from enum import IntEnum, Enum, auto
 from pynput import keyboard
 from threading import Thread
 
-# Using enum class create enumerations
-class Keys(Enum):
+class Key(Enum):
     KEY1 = auto()
     KEY2 = auto()
     KEY3 = auto()
@@ -12,62 +11,62 @@ class Keys(Enum):
     ROT_CW = auto()
     ROT_CCW = auto()
 
-keys = {
-    Keys.KEY1: '<F1>',
-    Keys.KEY2: '<F2>',
-    Keys.KEY3: '<F3>',
-    Keys.KEY4: '<F4>',
-    Keys.ROT_BUT: '<F5>',
-    Keys.ROT_CCW: '<F6>',
-    Keys.ROT_CW: '<F7>',
-}
+class KeypadError(Exception):
+    pass
 
-mode_modifiers = [
-    '<ctrl>', 
-    '<shift>',
-    '<ctrl>+<shift>'
-]
+class Keypad:
 
+    _keys = {
+        Key.KEY1: '<F1>',
+        Key.KEY2: '<F2>',
+        Key.KEY3: '<F3>',
+        Key.KEY4: '<F4>',
+        Key.ROT_BUT: '<F5>',
+        Key.ROT_CCW: '<F6>',
+        Key.ROT_CW: '<F7>',
+    }
 
-def _build_hotkeys():
+    _mode_modifiers = [
+        '<ctrl>', 
+        '<shift>',
+        '<ctrl>+<shift>'
+    ]
 
-    #hks = {}
+    def __init__(self, mode_enum: IntEnum):
+        self._mode_enum = mode_enum
+        
 
-    # for mode_idx, mode_mod in enumerate(mode_modifiers):
-    #     for key, key_value in keys.items():
-    #         print(f"{mode_idx}/{key.name}: \t{mode_mod}+{key_value}")
-    #         hks[f"{mode_mod}+{key_value}"] = handle_key(mode_idx,key)
+    def _build_hotkeys(self):
 
-    #return hks
-
-    return { 
-        f"{mode_mod}+{key_value}": _handle_key(mode_idx,key) 
-            for mode_idx, mode_mod in enumerate(mode_modifiers) 
-            for key, key_value in keys.items() 
-        }
-
-
-def _handle_key(mode: int, key: Keys):
-    def _handle_key_wrap():
-        Thread(target=_callback, args=(mode, key)).start()
-    return _handle_key_wrap  
+        try:
+            return { 
+                f"{self._mode_modifiers[mode.value]}+{key_value}": self._handle_key(mode, key) 
+                for mode in self._mode_enum 
+                for key, key_value in self._keys.items() 
+            }
+        except IndexError:
+            raise KeypadError(f"Too many modes defined, max={len(self._mode_modifiers)}")
 
 
-def start(callback):
-    global _callback    
-    _callback = callback
-
-    print ("Keyhooks Starting...")
-
-    hotkeys = _build_hotkeys()
-
-    global _listener
-    with keyboard.GlobalHotKeys(hotkeys) as h:
-        _listener = h
-        h.join()
+    def _handle_key(self, mode: IntEnum, key: Key):
+        def _handle_key_wrap():
+            Thread(target=self._callback, args=(mode, key)).start()
+        return _handle_key_wrap  
 
 
-def stop():
-    print ("Keyhooks Ending...")
-    global _listener
-    _listener.stop()
+    def start(self, callback):
+        
+        self._callback = callback
+
+        print ("Keyhooks Starting...")
+
+        with keyboard.GlobalHotKeys(self._build_hotkeys()) as h:
+            self._listener = h
+            h.join()
+
+
+    def stop(self):
+        print ("Keyhooks Ending...")
+        
+        if self._listener:
+            self._listener.stop()
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-import keypad
+from keypad import Keypad, Key
 import time
 
-def key_pressed(mode: int, key: keypad.Keys):
+from enum import IntEnum
+
+# Using enum class create enumerations
+class Mode(IntEnum):
+    Teams = 0
+    Sonos = 1
+    Another = 2
+
+
+keypad = Keypad(Mode)
+
+def key_pressed(mode: Mode, key: Key):
     print(f"START: mode: {mode}, Key: {key}")
     time.sleep(2)
     print(f"END: mode: {mode}, Key: {key}")
